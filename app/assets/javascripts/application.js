6eaf0d27f0bff6d251f0a7f4de493a385e296788
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-07-11 19:40:11 +0900

    [Finish]JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,24 @@
 //= require_tree .
 var title = "javascriptが使えました";
 alert(title);
+
+$(document).ready(function(){
+	$('.jquery').on('click', function(){
+		$(this).css('color', 'red');
+	});
+});
+
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    transition : 'slide',
+    speed : 1000,
+    easing : 'easeOutQuart',
+    navType : 'block',
+    childrenElementType : 'div',
+    arrows : true,
+    autoPlay : true,
+    autoPlayDuration : 3000,
+    keyboardOnAlways : true,
+    hidePrevious : false
+  });
+});
