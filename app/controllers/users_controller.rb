481c6907b19e13e2d2f581358e63696c8a1b2b2c
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-01 22:19:18 +0900

    [Finish]AdvancedWork4 Follow&Follower

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,20 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
+  def following
+    @user = User.find(params[:id])
+    @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
+
   private
+
   def user_params
   	params.require(:user).permit(:name, :profile_image, :introduction)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   	resources :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users, only: [:show, :edit, :update, :index] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
