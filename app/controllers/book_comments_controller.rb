e5a4e3f8a287df835517b8aba5af41e1513b752d
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-01 17:52:08 +0900

    [Update]Edited following the answer

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,20 @@
 class BookCommentsController < ApplicationController
 
+	before_action :authenticate_user!
+
 	def create
-		book = Book.find(params[:book_id])
-		comment = current_user.book_comments.new(book_comment_params)
-		comment.book_id = book.id
-		comment.save
-		redirect_back(fallback_location: root_path)
+		@book = Book.find(params[:book_id])
+		@book_comment = current_user.book_comments.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		if @book_comment.save
+			flash[:success] = "Comment was successfully created"
+		end
+		redirect_to request.referer
 	end
 
 	def destroy
 		BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-		redirect_back(fallback_location: root_path)
+		redirect_to request.referer
 	end
 
 	private
