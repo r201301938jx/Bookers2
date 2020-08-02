3388ef5d29f862166aea81088e5e7e7ed9411633
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-02 12:14:27 +0900

    [Finish]AdvancedWork5 Search

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
     end
   end
   resources :relationships, only: [:create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'search' => 'searches#search'
 end
